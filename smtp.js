75ea602217be83a559395ee7cba2db1311834bef
Kapex-Coder <kapex.coder@gmail.com>
2024-07-22 00:02:17 +0530

    Updated: Revert last commit

--- smtp.js
+++ smtp.js
@@ -3,8 +3,8 @@
 const handleContactForm = () => {
     Email.send({
         SecureToken: "9fbd48a0-687a-4010-87fa-1e2644e1e185",
-        From: "kapex.coder@gmail.com",
-        To: document.getElementById("user-email").value,
+        To: "kapex.coder@gmail.com",
+        From: document.getElementById("user-email").value,
         Subject: "This is the subject",
         Body: "And this is the body"
     }).then(
