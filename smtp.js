f3c24c03fb45958b13959baf267810cf796bbd85
Kapex-Coder <kapex.coder@gmail.com>
2024-07-22 00:29:50 +0530

    Updated: FormBody

--- smtp.js
+++ smtp.js
@@ -1,13 +1,22 @@
 // https://smtpjs.com/v3/smtp.js
 
 const handleContactForm = () => {
+    const name = document.getElementById("name").value;
+    const userEmail = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+    const formBody = `
+        Name: ${name} <br>
+        Email: ${userEmail} <br>
+        Message: ${message}
+    `
+
     Email.send({
         SecureToken: "9fbd48a0-687a-4010-87fa-1e2644e1e185",
         From: "kapex.coder@gmail.com",
         To: "kapex.coder@gmail.com",
-        ReplyFrom: document.getElementById("user-email").value,
-        Subject: "This is the subject",
-        Body: "And this is the body"
+        Subject: `Portfolio Contact Form: ${subject}`,
+        Body: formBody
     }).then(
         message => alert(message)
     );
