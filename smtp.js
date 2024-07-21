e3ce51a0b8453dd9ce3411bd3fc42033b134be3d
Kapex-Coder <kapex.coder@gmail.com>
2024-07-22 00:09:16 +0530

    Updated: ReplyFrom

--- smtp.js
+++ smtp.js
@@ -3,8 +3,9 @@
 const handleContactForm = () => {
     Email.send({
         SecureToken: "9fbd48a0-687a-4010-87fa-1e2644e1e185",
+        From: "kapex.coder@gmail.com",
         To: "kapex.coder@gmail.com",
-        From: document.getElementById("user-email").value,
+        ReplyFrom: document.getElementById("user-email").value,
         Subject: "This is the subject",
         Body: "And this is the body"
     }).then(
